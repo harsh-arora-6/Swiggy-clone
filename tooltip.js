ac8b7109542a480140a8d09f4313f2c9ca0742f2
Harsh Arora <harsh.arora@sprinklr.com>
2023-09-05 15:43:30 +0530

    Fixed minor bug of tooltip box

--- tooltip.js
+++ tooltip.js
@@ -5,26 +5,24 @@ const tooltipCart = document.querySelector(".cart .tooltip-box");
 /**
  * There is a small transition from link to tooltip
  *  That is why I have put slow appearance first time then
- *  hide takes 0.5s
+ *  hide-slow takes 0.5s
  *  but then when tooltip is entered fast showing is enabled (faster than hiding)
  * So it appears that tooltip is there the whole time
  **/
 
 cartLink.addEventListener("mouseenter", (e) => {
-  tooltipCart.classList.remove("hide");
-  const hasShowFast = tooltipCart.classList.contains("show-fast");
-  if (hasShowFast) return;
+  tooltipCart.classList.remove("hide-slow");
   tooltipCart.classList.add("show-slow");
 });
 
 cartLink.addEventListener("mouseleave", (e) => {
   tooltipCart.classList.remove("show-slow");
   tooltipCart.classList.remove("show-fast");
-  tooltipCart.classList.add("hide");
+  tooltipCart.classList.add("hide-slow");
 });
 
 tooltipCart.addEventListener("mouseenter", (e) => {
-  tooltipCart.classList.remove("hide");
+  tooltipCart.classList.remove("hide-slow");
   tooltipCart.classList.add("show-fast");
 
   cartLink.querySelector(".nav-item-text").classList.add("active-text");
@@ -34,7 +32,7 @@ tooltipCart.addEventListener("mouseenter", (e) => {
 
 tooltipCart.addEventListener("mouseleave", (e) => {
   tooltipCart.classList.remove("show-fast");
-  tooltipCart.classList.add("hide");
+  tooltipCart.classList.add("hide-slow");
 
   cartLink.querySelector(".nav-item-text").classList.remove("active-text");
   cartLink.querySelector(".nav-item-icon").classList.remove("active-cart-icon");
@@ -46,9 +44,7 @@ const tooltipSignIn = document.querySelector(".signin .tooltip-box");
 
 signInLink.addEventListener("mouseenter", (e) => {
   if (signInLink.getAttribute("loggedIn") === "false") return;
-  const hasShowFast = tooltipSignIn.classList.contains("show-fast");
-  if (hasShowFast) return;
-  tooltipSignIn.classList.remove("hide");
+  tooltipSignIn.classList.remove("hide-slow");
   tooltipSignIn.classList.add("show-slow");
 });
 
@@ -56,14 +52,14 @@ signInLink.addEventListener("mouseleave", (e) => {
   if (signInLink.getAttribute("loggedIn") === "false") return;
   tooltipSignIn.classList.remove("show-slow");
   tooltipSignIn.classList.remove("show-fast");
-  tooltipSignIn.classList.add("hide");
+  tooltipSignIn.classList.add("hide-slow");
 });
 
 tooltipSignIn.addEventListener("mouseenter", (e) => {
   signInLink.querySelector(".nav-item-text").classList.add("active-text");
   signInLink.querySelector(".nav-item-icon").classList.add("active-icon");
 
-  tooltipSignIn.classList.remove("hide");
+  tooltipSignIn.classList.remove("hide-slow");
   tooltipSignIn.classList.add("show-fast");
 });
 
@@ -72,5 +68,5 @@ tooltipSignIn.addEventListener("mouseleave", (e) => {
   signInLink.querySelector(".nav-item-icon").classList.remove("active-icon");
 
   tooltipSignIn.classList.remove("show-fast");
-  tooltipSignIn.classList.add("hide");
+  tooltipSignIn.classList.add("hide-slow");
 });
